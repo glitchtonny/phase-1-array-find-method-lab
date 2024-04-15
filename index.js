9430e405697e08d5c264137da6014e00d037dc84
g;itchtonny <sirmwangitosh@gmail.com>
2024-04-15 12:31:59 +0300

    find lab complete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // code your solution here
 function superbowlWin(records){
-    const win = records.find(record => record.result === "W");
+    const win = records.find(({result}) => result === "W");
     if (win){
         return win.year 
-    } else {undefined}
+    } else {
+        return undefined
+    }
 
 }
